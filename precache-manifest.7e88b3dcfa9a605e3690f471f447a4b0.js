d449e7566f17ceb69a68defa5b7c1f968d8f0082
Vikash Kumar <vikashsmart1996@gmail.com>
2020-07-07 22:37:08 +0530

    Updates

---
+++ precache-manifest.7e88b3dcfa9a605e3690f471f447a4b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "140b5af5c73096c249f4c472a5087a9e",
+    "url": "/multi_form/index.html"
+  },
+  {
+    "revision": "55f4802d2edd78b853a6",
+    "url": "/multi_form/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "535cb79448bf245e504e",
+    "url": "/multi_form/static/js/2.8cc722ae.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/multi_form/static/js/2.8cc722ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55f4802d2edd78b853a6",
+    "url": "/multi_form/static/js/main.81679907.chunk.js"
+  },
+  {
+    "revision": "7b124d6afe1e5581d4c1",
+    "url": "/multi_form/static/js/runtime-main.3c8aeee4.js"
+  }
+]);
